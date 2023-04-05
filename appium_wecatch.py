0e2a91e67c5a92e4615b755d2ec5713015aa4b12
naa4beebeeoh <26325741+naa4beebeeoh@users.noreply.github.co>
2023-04-05 09:58:22 +0800

    feat: send photo

--- appium_wecatch.py
+++ appium_wecatch.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import re
 import sys
@@ -21,42 +22,56 @@ capabilities = {
 prefix = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView"
 
 
-def send_to_telegram(message, api_token, chat_id):
+def send_to_telegram(message, file_opened, api_token, chat_id):
     try:
-        response = requests.post(
-            f"https://api.telegram.org/bot{api_token}/sendMessage",
-            json={"chat_id": chat_id, "text": message, "parse_mode": "HTML"},
-        )
+        if file_opened:
+            response = requests.post(
+                f"https://api.telegram.org/bot{api_token}/sendPhoto",
+                {"chat_id": chat_id, "caption": message, "parse_mode": "HTML"},
+                files={"photo": file_opened},
+            )
+        else:
+            response = requests.post(
+                f"https://api.telegram.org/bot{api_token}/sendMessage",
+                json={
+                    "chat_id": chat_id,
+                    "text": message,
+                    "parse_mode": "HTML",
+                },
+            )
         print(response.text)
     except Exception as e:
         print(e)
 
 
-def send_to_telegram_full(message):
+def send_to_telegram_full(message, file_opened):
     send_to_telegram(
         message,
+        file_opened,
         os.environ.get("TG_BOT_API_TOKEN"),
         "-1001920884298",
     )
 
 
-def send_to_telegram_hk(message):
+def send_to_telegram_hk(message, file_opened):
     send_to_telegram(
         message,
+        file_opened,
         os.environ.get("TG_BOT_API_TOKEN"),
         "-1001931323142",
     )
 
 
-def send_to_telegram_tw(message):
+def send_to_telegram_tw(message, file_opened):
     send_to_telegram(
         message,
+        file_opened,
         os.environ.get("TG_BOT_API_TOKEN"),
         "-1001804211083",
     )
 
 
-def check_if_send_to_telegram(el_name, el_address, msgs, func):
+def check_if_send_to_telegram(el_name, el_address, el_image, msgs, func):
     this_tuple = (el_name.text, el_address.text)
     print(this_tuple)
 
@@ -69,7 +84,13 @@ def check_if_send_to_telegram(el_name, el_address, msgs, func):
             r'座標: <a href="https://maps.google.com/?q=\1,\2">\1, \2</a>',
             f"{el_name.text}\n{el_address.text}\n\n",
         )
-        func(formatted_msg)
+
+        img_file = open("image.png", "wb")
+        img_file.write(base64.b64decode(el_image.screenshot_as_base64))
+        img_file.close()
+
+        with open("image.png", "rb") as img_file:
+            func(formatted_msg, img_file)
 
         msgs.append(this_tuple)
 
@@ -87,9 +108,9 @@ def run():
         )
         el.click()
 
-        send_to_telegram_full("=== 開機 ===")
-        send_to_telegram_hk("=== 開機 ===")
-        send_to_telegram_tw("=== 開機 ===")
+        send_to_telegram_full("=== 開機 ===", None)
+        send_to_telegram_hk("=== 開機 ===", None)
+        send_to_telegram_tw("=== 開機 ===", None)
 
         for y in range(1, 500):
             el = driver.find_element(
@@ -102,30 +123,48 @@ def run():
             time.sleep(30)
 
             for x in range(1, 4):
-                layout_xpath = f"{prefix}/android.widget.LinearLayout[{x}]/android.widget.LinearLayout/android.widget.LinearLayout"
+                layout_xpath = f"{prefix}/android.widget.LinearLayout[{x}]/android.widget.LinearLayout"
+                layout_2_xpath = f"{layout_xpath}/android.widget.LinearLayout"
 
                 el_name = driver.find_element(
                     by=AppiumBy.XPATH,
-                    value=f"{layout_xpath}/android.widget.TextView[1]",
+                    value=f"{layout_2_xpath}/android.widget.TextView[1]",
                 )
 
                 el_address = driver.find_element(
                     by=AppiumBy.XPATH,
-                    value=f"{layout_xpath}/android.widget.TextView[2]",
+                    value=f"{layout_2_xpath}/android.widget.TextView[2]",
+                )
+
+                el_image = driver.find_element(
+                    by=AppiumBy.XPATH,
+                    value=f"{layout_xpath}//android.widget.ImageView",
                 )
 
                 check_if_send_to_telegram(
-                    el_name, el_address, full_msgs, send_to_telegram_full
+                    el_name,
+                    el_address,
+                    el_image,
+                    full_msgs,
+                    send_to_telegram_full,
                 )
 
                 if "香港" in el_address.text:
                     check_if_send_to_telegram(
-                        el_name, el_address, hk_msgs, send_to_telegram_hk
+                        el_name,
+                        el_address,
+                        el_image,
+                        hk_msgs,
+                        send_to_telegram_hk,
                     )
 
                 elif "台灣" in el_address.text:
                     check_if_send_to_telegram(
-                        el_name, el_address, tw_msgs, send_to_telegram_tw
+                        el_name,
+                        el_address,
+                        el_image,
+                        tw_msgs,
+                        send_to_telegram_tw,
                     )
 
             # wait to view the list
@@ -141,9 +180,9 @@ def run():
 
     finally:
         driver.quit()
-        send_to_telegram_full("=== 關機 ===")
-        send_to_telegram_hk("=== 關機 ===")
-        send_to_telegram_tw("=== 關機 ===")
+        send_to_telegram_full("=== 關機 ===", None)
+        send_to_telegram_hk("=== 關機 ===", None)
+        send_to_telegram_tw("=== 關機 ===", None)
 
 
 run()
